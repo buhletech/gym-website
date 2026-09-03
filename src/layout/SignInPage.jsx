a7b0156e9dc0a9dd1493c3bb9596c247f04c7d8e
buhletech <brianbuhlem@gmail.com>
2026-09-03 22:36:14 +0200

    ...

--- src/layout/SignInPage.jsx
+++ src/layout/SignInPage.jsx
@@ -1,71 +1,64 @@
-import {useState} from "react";
-import NavBar from "../components/NavBar.jsx";
-import {ToastContainer} from "react-toastify";
-import Footer from "../components/Footer.jsx";
-import axios from "axios";
-import fs from "fs";
-import HomePage from "./HomePage.jsx";
+import { useState } from "react"
+import NavBar from "../components/NavBar.jsx"
+import { toast, ToastContainer } from "react-toastify"
+import Footer from "../components/Footer.jsx"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
 
 const SignInPage = () => {
-
-    const [selectedId, setSelectedId] = useState("");
-    const [selectedEmail, setSelectedEmail] = useState("");
+    const [selectedId, setSelectedId] = useState("")
+    const [selectedEmail, setSelectedEmail] = useState("")
+    const navigate = useNavigate()
 
     const handleIDChange = (event) => {
-        setSelectedId(event.target.value);
+        setSelectedId(event.target.value)
     }
 
     const handleEmailChange = (event) => {
-        setSelectedEmail(event.target.value);
+        setSelectedEmail(event.target.value)
     }
 
     const handleSubmitChange = (event) => {
-        event.preventDefault();
+        event.preventDefault()
 
-        const emailPath = 'C:\\Users\\Admin\\Documents\\gym-website\\src\\server\\data\\newUsers.json';
-        const data = JSON.parse(fs.readFileSync(emailPath, 'utf8'));
-
-        const emailExists = data.some(e => e.email === selectedEmail);
-        const idExists = data.some(e => e.idNo === selectedId);
-
-        if(emailExists || idExists) {
-            return 
+        if (selectedId.length !== 13) {
+            return toast("ID must be 13 digits")
         }
 
-        const submitObject = {
+        const userObject = {
             idNo: selectedId,
             email: selectedEmail
         }
 
-        axios.get('http://localhost:3001/api/newUsers', submitObject).then((res) => {
-
-
-        })
+        axios.post('http://localhost:3001/api/signin', userObject)
+            .then((res) => {
+                toast("Sign in successful")
+                navigate('/')
+            })
+            .catch((error) => {
+                if (error.response && error.response.status === 400) {
+                    toast(error.response.data.error)
+                }
+            })
     }
 
-    return(
+    return (
         <div className="sign-in-container">
-            <NavBar/>
+            <NavBar />
 
             <main>
                 <div className="sign-in-img">
-                    <svg>
-                        <path>
-
-                        </path>
-                    </svg>
+                    <svg><path></path></svg>
                 </div>
 
                 <div className="sign-in-body">
                     <div className="sign-in-header">
-                        <h3>Join Now</h3>
-
+                        <h3>Sign in</h3>
                         <p>Designed to add more value, more access, and more experiences to your training journey.</p>
                     </div>
 
                     <div className="sign-in-input">
                         <form className="signin-form-inline" onSubmit={handleSubmitChange}>
-
                             <div className="no-details">
                                 <label className="txt-label">ID No. / Passport No.</label>
                                 <input type="text" className="id-no" placeholder="ID No. / Passport No." value={selectedId} onChange={handleIDChange} required />
@@ -81,12 +74,12 @@ const SignInPage = () => {
                             </div>
                         </form>
 
-                        <ToastContainer/>
+                        <ToastContainer />
                     </div>
                 </div>
             </main>
 
-            <Footer/>
+            <Footer />
         </div>
     )
 }
